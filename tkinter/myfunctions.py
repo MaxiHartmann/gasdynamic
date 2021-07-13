0ed6ad806b5f115d64b111f2381dabd0336470e1
MaxiHartmann <maxi.hartmann207@gmail.com>
2021-07-13 19:21:47 +0200

    add menuWindow TODO: structure menuWindow.py

--- tkinter/myfunctions.py
+++ tkinter/myfunctions.py
@@ -2,34 +2,52 @@
 import numpy as np
 import math
 
-gamma = 1.4
-
-def calcMachnumber(Type, value):
+def calcMachnumber(Type, value, gamma=1.4):
     if (Type==0):
         machnumber = value;
     elif (Type==1):
         TdT0 = value;
+        if (TdT0 > 1.0):
+            print('T/T0 must be between 0 and 1')
+            return np.nan
         machnumber = np.sqrt(2. / (gamma - 1.) * (1. / TdT0 - 1.));
     elif (Type==2):
         pdp0 = value;
+        if (pdp0 > 1.0):
+            print('p/p0 must be between 0 and 1')
+            return np.nan
         machnumber = np.sqrt(2. / (gamma - 1.) *
             (np.power(pdp0, (1. - gamma) / gamma) - 1.));
     elif (Type==3):
         rhodrho0 = value;
+        if (rhodrho0 > 1.0):
+            print('rho/rho0 must be between 0 and 1')
+            return np.nan
         machnumber = np.sqrt(2. / (gamma - 1.) *
             (np.power(rhodrho0, (1. - gamma) / 1.) - 1.));
     elif (Type==4):
         AdAstar = value;
+        if (AdAstar < 1.0):
+            print('A/A* must be greater than 1')
+            return np.nan
         machnumber = findMaFromAratio(AdAstar, "sub");
     elif (Type==5):
         AdAstar = value;
+        if (AdAstar < 1.0):
+            print('A/A* must be greater than 1')
+            return np.nan
         machnumber = findMaFromAratio(AdAstar, "sup");
     elif (Type==6):
         MachAngle = value;
+        if (MachAngle >= 90.0):
+            print('Mach angle must be between 0 and 90')
+            return np.nan
         machnumber = 1. / np.sin(MachAngle * np.pi / 180.)
     elif (Type==7):
         PM_Angle = value;
-        # machnumber = findMaFromPM_Angle(PM_Angle)
+        if (PM_Angle >= 130.454076):
+            print('Prandtl-Meyer angle must be between 0 and 130.454076')
+            return np.nan
         # with regula_falsi:
         machnumber = findMaFromPM_Angle2(PM_Angle)
     else:
@@ -73,23 +91,6 @@ def findMaFromAratio(Aratio, flowType):
 
     return M
 
-# Doesn't work!!! Quadratic Function calc_AdAstar
-# def findMaFromAratio2(Aratio, flowType):
-#     Ma_min=0.
-#     Ma_max=0.
-#     if (flowType == "sub"):
-#         Ma_min = 1e-6;
-#         Ma_max = 0.999;
-#     if (flowType == "sup"):
-#         Ma_min  = 1.00001;
-#         Ma_max  = 30.0;
-#     iterations=30
-#     tolerance=1e-6
-#     print("Ma_min = {}, Ma_max={}, iterations={}, tol={}, Aratio={}".format(Ma_min, Ma_max,iterations,tolerance,Aratio))
-#     Ma = regula_falsi(calc_AdAstar, Ma_min, Ma_max,iterations,
-#             tolerance, Aratio)
-#     return Ma
-
 def findMaFromPM_Angle(PM_Angle):
     dM = 0.1
     M = 1.0
@@ -122,40 +123,40 @@ def findMaFromPM_Angle(PM_Angle):
 def findMaFromPM_Angle2(PM_Angle):
     Ma_min=1.0
     Ma_max=50.0
-    Ma = regula_falsi(calc_PM_Angle, Ma_min, Ma_max,30, 1e-6, PM_Angle)
+    Ma = regula_falsi(calc_PM_Angle, Ma_min, Ma_max,30, 1e-8, PM_Angle)
     return Ma
 
-def calc_AdAstar(Ma):
+def calc_AdAstar(Ma, gamma=1.4):
     result = 1. / (Ma * np.power((2. / (gamma + 1.) *
         (1. + (gamma - 1.) / 2. * Ma * Ma)),
         (-(gamma + 1.) / (2. * gamma -2.))))
     return result
 
-def calc_TdT0(Ma):
+def calc_TdT0(Ma, gamma=1.4):
     result = (1 + (gamma - 1) / 2 * Ma**2)**(-1)
     return result
 
-def calc_MaStar(Ma):
+def calc_MaStar(Ma, gamma=1.4):
     result = np.sqrt(( gamma + 1.)/ (2. / (Ma*Ma) + gamma -1.))
     return result
 
-def calc_pdp0(Ma):
+def calc_pdp0(Ma, gamma=1.4):
     result = (1 + (gamma - 1) / 2 * Ma**2)**(-gamma / (gamma - 1))
     return result
 
-def calc_rhodrho0(Ma):
+def calc_rhodrho0(Ma, gamma=1.4):
     result = (1 + (gamma - 1) / 2 * Ma**2)**(-1 / (gamma - 1))
     return result
 
 def calc_MachAngle(Ma):
-    if(Ma<1.0):
+    if(Ma < 1.0):
         print("Ma < 1.0: no P-M angle in subsonic flow")
         return np.nan
     result = np.arcsin( 1. / Ma) * 180. / np.pi
     return result
 
-def calc_PM_Angle(Ma):
-    if(Ma<1.0):
+def calc_PM_Angle(Ma, gamma=1.4):
+    if(Ma < 1.0):
         print("Ma < 1.0: no Mach angle in subsonic flow")
         return np.nan
     result = (np.sqrt((gamma + 1.) / (gamma - 1.))
@@ -164,7 +165,7 @@ def calc_PM_Angle(Ma):
     return result
 
 
-def regula_falsi(func, a, b, max_steps=100, tolerance=1e-5, target=0.):
+def regula_falsi(func, a, b, max_steps=100, tolerance=1e-6, target=0.):
     '''Calculate x for (f(x)-target)=0 of a function:
     Inputs:
         func: Function(x)
@@ -183,13 +184,13 @@ def regula_falsi(func, a, b, max_steps=100, tolerance=1e-5, target=0.):
         func_a = func(a) - target
         func_b = func(b) - target
         x = b - (func_b * ((a-b)/(func_a-func_b)))
-        # print("Current approximation is {:.05f}".format(x))
+        # print("Current approximation is {:.08f}".format(x))
         if math.copysign(func_a, func_b) != func_a:
             b = x
         else:
             a = x
         if abs(func(x)-target) < tolerance:
-            # print ("Root is {:.05f} ({} iterations)".format(x, loopCount))
+            print ("Root is {:.08f} ({} iterations)".format(x, loopCount))
             return x
-    # print("Root find cancelled at {:.05f} ({} iterations)".format(x, loopCount))
+    print("Root find cancelled at {:.08f} ({} iterations)".format(x, loopCount))
     return x
