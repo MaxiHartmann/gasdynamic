6c9bb448ce847b07ed212c7fa901cd2911e275ab
MaxiHartmann <maxi.hartmann207@gmail.com>
2021-07-16 15:27:50 +0200

    template for tkinter structer should be working

--- tkinter/small_structure.py
+++ tkinter/small_structure.py
@@ -47,14 +47,10 @@ class SeaofBTCapp(tk.Tk):
 
 # Example: How to reach tk-widgets from outside of class:
 def reset(self):
-    print(self)
     self.led_2.delete(0,tk.END)
     self.led_3.delete(0,tk.END)
     self.led_4.delete(0,tk.END)
 
-def testFunction(self):
-    print("TEST-Function")
-    self.testLabel.config(text="xxx")
 
 
 class StartPage(tk.Frame):
@@ -109,7 +105,7 @@ class PageOne(tk.Frame):
         #====================Variables ====================  
         txt_inputValue = tk.StringVar(value="2.0")
         txt_gamma = tk.StringVar(value="1.4")
-        my_dict1={0: 'Machnumber', 
+        self.my_dict1={0: 'Machnumber', 
                   1: 'T/T0', 
                   2: 'p/p0', 
                   3: 'rho/rho0',
@@ -117,86 +113,49 @@ class PageOne(tk.Frame):
                   5: 'A/A*(sup)',
                   6: 'Mach angle (deg.)',
                   7: 'P-M angle (deg.)'}
-        keys=list(my_dict1.keys())
-        values=my_dict1.values()
+        keys=list(self.my_dict1.keys())
+        values=self.my_dict1.values()
 
-        txt_inputType = tk.StringVar(inputFrame, value=my_dict1[0])
+        self.txt_inputType = tk.StringVar(inputFrame, value=self.my_dict1[0])
 
         # TEST:
-        self.testButton = tk.Button(self, text="TEST :::", bg='yellow', 
-                command=lambda: testFunction(self))
-        # self.testButton.bind('<Button-1>', lambda: testFunction(self, event))
+        self.testButton = tk.Button(self, text="TEST :::", bg='yellow')
+                # command=lambda: testFunction(self))
+        self.testButton.bind('<Button-1>', self.testFunction)
         self.testButton.pack()
         self.testLabel = tk.Label(self, text="TEST :::", bg='yellow')
         self.testLabel.pack()
 
-        #====================Methods ====================  
-        def calc():
-            print("reset and Calc")
-            reset()
-
-            # get idx in OptionMenu:
-            inputType = get_idx_fromOptionMenu(txt_inputType.get())
-            inputValue=float(led_1.get())
-            gamma=float(led_gamma.get())
-
-            Ma = calcMachnumber(inputType, inputValue, gamma)
-            pdp0 = calc_pdp0(Ma,gamma)
-            MachAngle = calc_MachAngle(Ma)
-            rhodrho0 = calc_rhodrho0(Ma, gamma)
-            TRatio = calc_TdT0(Ma,gamma)
-            PM_angle = calc_PM_Angle(Ma,gamma)
-            MaStar = calc_MaStar(Ma, gamma)
-            AdAstar = calc_AdAstar(Ma, gamma)
-            rho_rhoStar = 1. / AdAstar * (1./MaStar)
-            T_Tstar = 1. / (Ma / MaStar)**2
-            pdpstar = rho_rhoStar * T_Tstar
-
-            # column=0
-            self.led_2.insert(0,"{:.07f}".format(Ma))
-            self.led_3.insert(0,"{:.07f}".format(pdp0))
-            self.led_4.insert(0,"{:.07f}".format(pdpstar))
-
-        def get_idx_fromOptionMenu(search_key):
-            for idx, option in my_dict1.items():
-                if option == search_key:
-                    # print("idx = {}, search_key= {}".format(idx, option))
-                    return idx
-
-        # def reset():
-        #     self.led_2.delete(0,tk.END)
-        #     self.led_3.delete(0,tk.END)
-        #     self.led_4.delete(0,tk.END)
-
         #====================INPUT Line====================  
-        lbl_name = tk.Label(inputFrame, text="Isentropic Flow Calculator",
+        self.lbl_name = tk.Label(inputFrame, text="Isentropic Flow Calculator",
                 bg='green', font=('arial', 20, 'bold'))
-        lbl_name.grid(row=0, column=0)
-        lbl_input = tk.Label(inputFrame, text="INPUT:")
-        lbl_input.grid(row=1, column=0, sticky=tk.E)
-        lsbox_1 = tk.OptionMenu(inputFrame, txt_inputType, *my_dict1.values())
-        lsbox_1.config(width=16)
-        lsbox_1.grid(row=1, column=1)
-        lbl_input = tk.Label(inputFrame, text="gamma = ")
-        lbl_input.grid(row=0, column=2, sticky=tk.E)
-        led_gamma = tk.Entry(inputFrame, textvariable=txt_gamma, width=8)
-        led_gamma.grid(row=0, column=3)
+        self.lbl_name.grid(row=0, column=0)
+        self.lbl_input = tk.Label(inputFrame, text="INPUT:")
+        self.lbl_input.grid(row=1, column=0, sticky=tk.E)
+        self.lsbox_1 = tk.OptionMenu(inputFrame, self.txt_inputType, *self.my_dict1.values())
+        self.lsbox_1.config(width=16)
+        self.lsbox_1.grid(row=1, column=1)
+        self.lbl_input = tk.Label(inputFrame, text="gamma = ")
+        self.lbl_input.grid(row=0, column=2, sticky=tk.E)
+        self.led_gamma = tk.Entry(inputFrame, textvariable=txt_gamma, width=8)
+        self.led_gamma.grid(row=0, column=3)
         
-        led_1 = tk.Entry(inputFrame, textvariable=txt_inputValue, width=10)
-        led_1.grid(row=1, column=2)
-        btn_calc = tk.Button(inputFrame, text="Calculate", fg="red", 
-                command=calc)
-        btn_calc.grid(row=1, column=3)
+        self.led_1 = tk.Entry(inputFrame, textvariable=txt_inputValue, width=10)
+        self.led_1.grid(row=1, column=2)
+        self.btn_calc = tk.Button(inputFrame, text="Calculate", fg="red")
+        self.btn_calc.bind('<Button-1>', self.calc)
+        self.btn_calc.grid(row=1, column=3)
         self.btn_reset = tk.Button(inputFrame, text="RESET", fg="red", 
                 command=lambda: reset(self))
         self.btn_reset.grid(row=2, column=3)
+
         #====================Results Frame====================  
-        label2 = tk.Label(resultsFrame, text="Machnumber =")
-        label2.grid(row=0, column=0, sticky=tk.E)
-        label3 = tk.Label(resultsFrame, text="p/p0 =")
-        label3.grid(row=1, column=0, sticky=tk.E)
-        label4 = tk.Label(resultsFrame, text="p/p* =")
-        label4.grid(row=2, column=0, sticky=tk.E)
+        self.label2 = tk.Label(resultsFrame, text="Machnumber =")
+        self.label2.grid(row=0, column=0, sticky=tk.E)
+        self.label3 = tk.Label(resultsFrame, text="p/p0 =")
+        self.label3.grid(row=1, column=0, sticky=tk.E)
+        self.label4 = tk.Label(resultsFrame, text="p/p* =")
+        self.label4.grid(row=2, column=0, sticky=tk.E)
 
         self.led_2 = tk.Entry(resultsFrame, text="", justify='right', width=12)
         self.led_2.grid(row=0, column=1)
@@ -204,6 +163,43 @@ class PageOne(tk.Frame):
         self.led_3.grid(row=1, column=1)
         self.led_4 = tk.Entry(resultsFrame, text="", justify='right', width=12)
         self.led_4.grid(row=2, column=1)
+
+    #====================Methods ====================  
+    def calc(self,event):
+        print("reset and Calc")
+
+        # get idx in OptionMenu:
+        inputType = self.get_idx_fromOptionMenu(self.txt_inputType.get())
+        inputValue=float(self.led_1.get())
+        gamma=float(self.led_gamma.get())
+
+        Ma = calcMachnumber(inputType, inputValue, gamma)
+        pdp0 = calc_pdp0(Ma,gamma)
+        MachAngle = calc_MachAngle(Ma)
+        rhodrho0 = calc_rhodrho0(Ma, gamma)
+        TRatio = calc_TdT0(Ma,gamma)
+        PM_angle = calc_PM_Angle(Ma,gamma)
+        MaStar = calc_MaStar(Ma, gamma)
+        AdAstar = calc_AdAstar(Ma, gamma)
+        rho_rhoStar = 1. / AdAstar * (1./MaStar)
+        T_Tstar = 1. / (Ma / MaStar)**2
+        pdpstar = rho_rhoStar * T_Tstar
+
+        # column=0
+        self.led_2.insert(0,"{:.07f}".format(Ma))
+        self.led_3.insert(0,"{:.07f}".format(pdp0))
+        self.led_4.insert(0,"{:.07f}".format(pdpstar))
+
+    def get_idx_fromOptionMenu(self, search_key):
+        for idx, option in self.my_dict1.items():
+            if option == search_key:
+                # print("idx = {}, search_key= {}".format(idx, option))
+                return idx
+
+    def testFunction(self, event):
+        print("TEST-Function")
+        self.testLabel.config(text="xxx")
+
 
 #================================================================================
 #==========      Page Two        ==================================================
@@ -273,27 +269,6 @@ class PageSix(tk.Frame):
                 command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-# Nice: Live Plotting
-# class PageThree(tk.Frame):
-# 
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#         label = tk.Label(self, text="Graph Page", font=LARGE_FONT)
-#         label.pack(pady=10, padx=10)
-# 
-#         button1 = ttk.Button(self, text="Back to Home",
-#                 command=lambda: controller.show_frame(StartPage))
-#         button1.pack()
-# 
-#         canvas = FigureCanvasTkAgg(f, self)
-#         canvas.draw()
-#         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-# 
-#         toolbar = NavigationToolbar2Tk(canvas, self)
-#         toolbar.update()
-#         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-# 
-# ani = animation.FuncAnimation(f, animate, interval=1000)
-
-app = SeaofBTCapp()
-app.mainloop()
+if __name__ == "__main__":
+    app = SeaofBTCapp()
+    app.mainloop()
